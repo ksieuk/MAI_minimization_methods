9b31f8f452c54501b386e0b9ce6bc98980b377a6
ksieuk <iebebd3@gmail.com>
2023-06-23 13:16:14 +0300

    initial

---
+++ methods/first_part/min.py
@@ -0,0 +1,109 @@
+import math
+import numpy as np
+
+
+def f(x, f_num):
+    if f_num == 1:
+        return (np.sin(x)) ** 11
+    else:
+        return 2 * (x - 1) ** 2 + 0.01 / (1 - 2 * x ** 2)
+
+
+def uniform_search(a, b, epsilon, f_num, n):
+    step = (b - a) / n
+    if step < epsilon:
+        print("Количество шагов слишком велико для заданной точности")
+        return None
+    min_func = f(a, f_num)
+    min_arg = a
+
+    i = 0
+    for k in range(int(n)):
+        x = a + k * step
+        fx = f(x, f_num)
+        if fx < min_func:
+            min_func = fx
+            min_arg = x
+        i += 1
+
+    return [min_arg - step, min_arg + step], i
+
+
+def bisection_search(a, b, epsilon, f_num, n):
+    i = 0
+    while abs(b - a) > epsilon:
+        x = (a + b) / 2
+        if derivative(f_num, x) > 0:
+            b = x
+        else:
+            a = x
+        i += 1
+
+    return (a + b) / 2, i
+
+
+def derivative(f_num, x, h=1e-7):
+    return (f(x + h, f_num) - f(x - h, f_num)) / (2 * h)
+
+
+def golden_section_search(a, b, epsilon, f_num, n):
+    i = 0
+    golden_ratio = (1 + math.sqrt(5)) / 2
+    c = b - (b - a) / golden_ratio
+    d = a + (b - a) / golden_ratio
+
+    while abs(c - d) > epsilon:
+        if f(c, f_num) < f(d, f_num):
+            b = d
+        else:
+            a = c
+        i += 1
+
+        c = b - (b - a) / golden_ratio
+        d = a + (b - a) / golden_ratio
+    return (b + a) / 2, i
+
+
+algorithms_ru = {
+    "1": "Равномерный поиск",
+    "2": "Метод половинного деления",
+    "3": "Метод золотого сечения",
+}
+
+algorithm_funcs = {
+    "1": uniform_search,
+    "2": bisection_search,
+    "3": golden_section_search,
+}
+
+
+def start_algorithm(a, b, epsilon, algorithm_type, f_num, n):
+    minimum, iteration_number = algorithm_funcs[algorithm_type](a, b, epsilon, f_num, n)
+
+    print(f"Минимум функции на заданном интервале находится в точке x = {minimum},"
+          f" количество итераций = {iteration_number}")
+
+
+def start_input():
+    for key, value in algorithms_ru.items():
+        print(f"{key}: {value}")
+    algorithm_number = input('\nВыберите алгоритм поиска. Укажите номер алгоритма: ')
+
+    if algorithm_number not in algorithms_ru:
+        raise ValueError('Такого алгоритма поиска не существует')
+
+    f_num = int(input("Введите номер функции (1 или 2): "))
+    a = float(input("Введите левую границу интервала a: "))
+    b = float(input("Введите правую границу интервала b: "))
+    epsilon = float(input("Введите точность поиска epsilon: "))
+    n = int(input("Введите количество шагов n: "))
+
+    start_algorithm(a, b, epsilon, algorithm_number, f_num, n)
+
+
+def main():
+    start_input()
+
+
+if __name__ == '__main__':
+    main()
---
+++ methods/first_part/unimod_steps.py
@@ -0,0 +1,108 @@
+import numpy as np
+
+
+def f(x, f_num):
+    if f_num == '1':
+        return (np.sin(x)) ** 11
+    else:
+        return 2 * (x - 1) ** 2 + 0.01 / (1 - 2 * x ** 2)
+
+
+def const_step(x, k, xp, delta, f_num):
+    xk = x + delta
+    if f(xk, f_num) > f(x, f_num):
+        a = xp
+        b = xk
+        print(f'[{a:.3f}; {b:.3f}] {(b - a):.3f}, k = {k}')
+        return a, b
+    return const_step(x=xk, k=k + 1, xp=x, delta=delta, f_num=f_num)
+
+
+def prop_step(x, k, xp, delta, f_num):
+    xk = x + k * delta
+    if f(xk, f_num) > f(x, f_num):
+        a = xp
+        b = xk
+        print(f'[{a:.3f}; {b:.3f}] {(b - a):.3f}, k = {k}')
+        return a, b
+    return prop_step(x=xk, k=k + 1, xp=x, delta=delta, f_num=f_num)
+
+
+def svenn_step(x, k, xp, delta, f_num):
+    xk = x + pow(2, k) * delta
+    if f(xk, f_num) > f(x, f_num):
+        a = xp
+        b = xk
+        print(f'[{a:.3f}; {b:.3f}] {(b - a):.3f}, k = {k}')
+        return a, b
+    return svenn_step(x=xk, k=k + 1, xp=x, delta=delta, f_num=f_num)
+
+
+def unimod(x, delta, a, b, f_num, algorithm_type):
+    fa = f(x, f_num)
+    fb = f(x + delta, f_num)
+    fc = f(x - delta, f_num)
+
+    if fc >= fa <= fb:
+        return (a + b) / 2
+
+    if fc <= fa <= fb:
+        delta = -delta
+    a, b = step(x=x, k=0, xp=x, delta=delta, algorithm_type=algorithm_type, f_num=f_num)
+
+    return (a + b) / 2
+
+
+def step(x, k, xp, delta, f_num, algorithm_type):
+    if algorithm_type == '1':
+        return const_step(x, k, xp, delta, f_num)
+    elif algorithm_type == '2':
+        return prop_step(x, k, xp, delta, f_num)
+    else:
+        return svenn_step(x, k, xp, delta, f_num)
+
+
+algorithm_names = {
+    "1": "С постоянным шагом",
+    "2": "С пропорциональным шагом",
+    "3": "С шагом Свенна",
+}
+
+
+def start_algorithm(x0, a, b, delta, ran, f_num, algorithm_type):
+    result = 0
+    for i in range(ran):
+        result = unimod(x0, delta, a, b, f_num, algorithm_type)
+        delta += 0.001
+        print(i + 1, end=': ')
+    print(
+        f"Интервал унимодальности находится в точке {result:.6f}, значение функции в этой точке {f(result, f_num):.6f}")
+
+
+def start_input():
+    for key, value in algorithm_names.items():
+        print(f"{key}: {value}")
+
+    algorithm_number = input('\nВыберите алгоритм поиска. Укажите номер алгоритма: ')
+    if algorithm_number not in algorithm_names:
+        raise ValueError('Такого алгоритма поиска не существует')
+
+    f_num = input("Введите номер функции (1 или 2): ")
+    if f_num not in ("1", "2"):
+        raise ValueError('Такого номера функции не существует')
+
+    x0 = float(input("Введите начальную точку: "))
+    a = float(input("Введите левую границу области поиска: "))
+    b = float(input("Введите правую границу области поиска: "))
+    delta = float(input("Введите величину параметра шага: "))
+    ran = int(input("Введите количество итераций: "))
+
+    start_algorithm(x0, a, b, delta, ran, f_num, algorithm_number)
+
+
+def main():
+    start_input()
+
+
+if __name__ == '__main__':
+    main()
